3382a078bf421e76a96401669a8aeb6c85e56c8c
Amal C.S <amal4cs@gmail.com>
2022-04-08 22:36:07 +0530

    Refactor

--- src/io/io_service.cpp
+++ src/io/io_service.cpp
@@ -2,3 +2,92 @@
 
 thread_local unsigned int io_service::m_thread_id                = 0;
 thread_local uring_allocator *io_service::m_uring_data_allocator = nullptr;
+
+io_service::io_service(const u_int &entries, const u_int &flags)
+    : m_entries(entries), m_flags(flags) {
+  io_uring_queue_init(entries, &m_uring, flags);
+  m_io_cq_thread = std::move(std::thread([&] { this->io_loop(); }));
+
+  m_io_queues.resize(32);
+  for (size_t i = 0; i < 32; ++i) {
+    m_io_queues[i] = new io_op_pipeline(128);
+  }
+}
+
+io_service::~io_service() { io_uring_queue_exit(&m_uring); }
+
+unsigned int io_service::drain_io_results() {
+  unsigned completed = 0;
+  unsigned head;
+  struct io_uring_cqe *cqe;
+
+  io_uring_for_each_cqe(&m_uring, head, cqe) {
+    ++completed;
+    auto data = static_cast<uring_data *>(io_uring_cqe_get_data(cqe));
+    if (data != nullptr) {
+      data->m_result = cqe->res;
+      data->m_flags  = cqe->flags;
+      if (data->m_handle_ctl.exchange(true, std::memory_order_acq_rel)) {
+        data->m_scheduler->schedule(data->m_handle);
+      }
+    }
+  }
+  if (completed) {
+    io_uring_cq_advance(&m_uring, completed);
+  }
+  return completed;
+}
+
+void io_service::io_loop() noexcept {
+  while (true) {
+    io_uring_cqe *cqe = nullptr;
+    if (io_uring_wait_cqe(&m_uring, &cqe) == 0) {
+      auto data = static_cast<uring_data *>(io_uring_cqe_get_data(cqe));
+      if (data != nullptr) {
+        data->m_result = cqe->res;
+        data->m_flags  = cqe->flags;
+        if (data->m_handle_ctl.exchange(true, std::memory_order_acq_rel)) {
+          data->m_scheduler->schedule(data->m_handle);
+        }
+      }
+      io_uring_cqe_seen(&m_uring, cqe);
+    } else {
+      std::cerr << "Wait CQE Failed\n";
+    }
+
+    drain_io_results();
+  }
+  return;
+}
+
+bool io_service::io_queue_empty() const noexcept {
+  bool is_empty = true;
+  for (auto &q : this->m_io_queues) {
+    is_empty = is_empty && q->empty();
+  }
+  return is_empty;
+}
+
+void io_service::setup_uring_allocator() {
+  if (m_thread_id == 0) {
+    m_thread_id = m_io_queue_count.fetch_add(1, std::memory_order_relaxed) + 1;
+    m_uring_data_allocator = new uring_allocator;
+    m_uring_data_allocators.push_back(m_uring_data_allocator);
+  }
+}
+
+void io_service::submit_pending_io() {
+  while (!io_queue_empty() &&
+         !m_io_sq_running.exchange(true, std::memory_order_seq_cst)) {
+
+    while (!io_queue_empty()) {
+      for (auto &q : m_io_queues) {
+        if (q->init_io_uring_ops(&m_uring)) {
+          io_uring_submit(&m_uring);
+        }
+      }
+    }
+
+    m_io_sq_running.store(false, std::memory_order_relaxed);
+  }
+}
--- src/io/io_service.hpp
+++ src/io/io_service.hpp
@@ -30,46 +30,9 @@ class io_service {
   std::atomic_int m_io_queue_count{0};
 
 public:
-  io_service(const u_int &entries, const u_int &flags)
-      : m_entries(entries), m_flags(flags) {
-    io_uring_queue_init(entries, &m_uring, flags);
-    m_io_cq_thread = std::move(std::thread([&] { this->io_loop(); }));
+  io_service(const u_int &entries, const u_int &flags);
 
-    m_io_queues.resize(32);
-    for (size_t i = 0; i < 32; ++i) {
-      m_io_queues[i] = new io_op_pipeline(128);
-    }
-  }
-
-  auto submit_io(IO_URING_OP auto &&operation) -> uring_awaiter {
-
-    if (m_thread_id == 0) {
-      m_thread_id =
-          m_io_queue_count.fetch_add(1, std::memory_order_relaxed) + 1;
-      m_uring_data_allocator = new uring_allocator;
-      m_uring_data_allocators.push_back(m_uring_data_allocator);
-    }
-
-    auto future = operation.get_future(m_uring_data_allocator);
-
-    m_io_queues[m_thread_id - 1]->enqueue(operation);
-
-    while (!io_queue_empty() &&
-           !m_io_sq_running.exchange(true, std::memory_order_seq_cst)) {
-
-      while (!io_queue_empty()) {
-        for (auto &q : m_io_queues) {
-          if (q->init_io_uring_ops(&m_uring)) {
-            io_uring_submit(&m_uring);
-          }
-        }
-      }
-
-      m_io_sq_running.store(false, std::memory_order_relaxed);
-    }
-
-    return future;
-  }
+  ~io_service();
 
   auto openat(const int &dfd, const char *const &filename, const int &flags,
               const mode_t &mode) -> uring_awaiter {
@@ -110,57 +73,26 @@ public:
   }
 
 protected:
-  unsigned int drain_io_results() {
-    unsigned completed = 0;
-    unsigned head;
-    struct io_uring_cqe *cqe;
+  auto submit_io(IO_URING_OP auto &&operation) -> uring_awaiter {
 
-    io_uring_for_each_cqe(&m_uring, head, cqe) {
-      ++completed;
-      auto data = static_cast<uring_data *>(io_uring_cqe_get_data(cqe));
-      if (data != nullptr) {
-        data->m_result = cqe->res;
-        data->m_flags  = cqe->flags;
-        if (data->m_handle_ctl.exchange(true, std::memory_order_acq_rel)) {
-          data->m_scheduler->schedule(data->m_handle);
-        }
-      }
-    }
-    if (completed) {
-      io_uring_cq_advance(&m_uring, completed);
-    }
-    return completed;
+    setup_uring_allocator();
+
+    auto future = operation.get_future(m_uring_data_allocator);
+
+    m_io_queues[m_thread_id - 1]->enqueue(operation);
+
+    return future;
   }
 
-  void io_loop() noexcept {
-    while (true) {
-      io_uring_cqe *cqe = nullptr;
-      if (io_uring_wait_cqe(&m_uring, &cqe) == 0) {
-        auto data = static_cast<uring_data *>(io_uring_cqe_get_data(cqe));
-        if (data != nullptr) {
-          data->m_result = cqe->res;
-          data->m_flags  = cqe->flags;
-          if (data->m_handle_ctl.exchange(true, std::memory_order_acq_rel)) {
-            data->m_scheduler->schedule(data->m_handle);
-          }
-        }
-        io_uring_cqe_seen(&m_uring, cqe);
-      } else {
-        std::cerr << "Wait CQE Failed\n";
-      }
+  unsigned int drain_io_results();
 
-      drain_io_results();
-    }
-    return;
-  }
+  void io_loop() noexcept;
 
-  bool io_queue_empty() {
-    bool is_empty = true;
-    for (auto &q : this->m_io_queues) {
-      is_empty = is_empty && q->empty();
-    }
-    return is_empty;
-  }
+  bool io_queue_empty() const noexcept;
+
+  void setup_uring_allocator();
+
+  void submit_pending_io();
 };
 
 #endif
